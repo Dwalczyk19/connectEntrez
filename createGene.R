791122b5219b0069618c11973b4f34ae2eea8832
Dwalczyk19 <92831596+Dwalczyk19@users.noreply.github.com>
2023-07-05 17:22:42 -0400

    Update createGene.R

--- createGene.R
+++ createGene.R
@@ -1,6 +1,3 @@
-#Parse out and connect exons 
-#find which indices of exon contain the same gene
-
 library(seqinr)
 library(Biostrings)
 library(coRdon)
@@ -14,11 +11,21 @@ reading = read.fasta(file_path)
 #goal is to merge exons together from each 
 reading[1]
 
+gene <- c()
 for ( i in names(reading)) {
   string <- attr(reading[[i]], "Annot")
   split <- unlist(strsplit(string, " "))
   gene <- append(gene,split[2])
 }
+
+for ( x in unique(gene)) {
+  check <- which(gene == x)
+  print(x)
+  print(check)
+  check <- c()
+}
+
+
 
 
 codonTable(DNAStringSet(paste(as.character(reading$`NC_060935.1:c66310697-66308147`), collapse = "")))
@@ -32,4 +39,6 @@ v <- c('A','B','C','D')
 print(v)
 print(paste(v,collapse=''))
 print(paste(v))
+
+
 
