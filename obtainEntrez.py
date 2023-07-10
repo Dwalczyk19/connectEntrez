32faaf09dee9d0ba97fe9a1dba6b49a058b0c226
Dwalczyk19 <92831596+Dwalczyk19@users.noreply.github.com>
2023-07-10 15:56:47 -0400

    Update obtainEntrez.py

--- obtainEntrez.py
+++ obtainEntrez.py
@@ -1,4 +1,3 @@
-
 import ncbi.datasets.openapi    
 from ncbi.datasets.openapi.api import gene_api
 from ncbi.datasets.openapi import ApiClient as DatasetsApiClient
@@ -12,7 +11,7 @@ import regex as re
 
 
 # Provide your own gene ids as a list of integers
-read = pd.read_excel("~/connectEntrez-main/data/Human_genes_with_entrez_IDs060523.xlsx")
+read = pd.read_excel("/home/dwalz/R/data/Human_genes_with_entrez_IDs060523.xlsx")
 read = pd.DataFrame(read)
 entrez_id = read["NEW-Entrez-ID"]
 gene_id = read["NEW-Gene-ID"]
@@ -39,9 +38,10 @@ def example_usage_of_api(gene_ids):
         try:
             print("Begin download of data package ...")
             gene_ds_download = gene_api.download_gene_package(
-                gene_ids, include_annotation_type=["FASTA_GENE"], _preload_content=False   #FASTA_PROTEIN, FASTA_RNA, etc.
+                gene_ids, include_annotation_type=["FASTA_CDS"], _preload_content=False   #FASTA_PROTEIN, FASTA_RNA, etc.
             )
             data = gene_ds_download.data
+           
             #store = "ncbi_dataset/data/data_report.jsonl".join(str(data).split("ncbi_dataset/data/data_report.jsonl")[:2])
             #print(store.decode("utf-8"))
             
@@ -52,7 +52,7 @@ def example_usage_of_api(gene_ids):
 
         except DatasetsApiException as e:
             print(f"Exception when calling GeneApi: {e}\n")
-            
+
 
 n = int(input("How many genes would you like to access, press 0 to quit --> "))
 if n > 0: 
@@ -61,7 +61,6 @@ elif n == 0:
     print("Finished...")
 else: 
     n = int(input("How many genes would you like to access, press 0 to quit --> "))
-    
     
 
 
